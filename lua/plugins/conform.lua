e02115bb7184f87200529fd52472fdfe224f748b
Jad Haddad <jad.haddad92@gmail.com>
2024-04-16 18:05:28 +0200

    Update plugins

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,44 +1,51 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  keys = {
-    {
-      -- Customize or remove this keymap to your liking
-      "<leader>f",
-      function()
-        require("conform").format({ async = true, lsp_fallback = true })
-      end,
-      mode = "",
-      desc = "Format buffer",
-    },
-  },
-  -- Everything in opts will be passed to setup()
-  opts = {
-    -- Define your formatters
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = {
-        -- To fix lint errors.
-        "ruff_fix",
-        -- To run the Ruff formatter.
-        "ruff_format",
-      },
-    },
-    -- Set up format-on-save
-    format_on_save = { timeout_ms = 500, lsp_fallback = true },
-    -- Customize formatters
-    formatters = {
-      shfmt = {
-        prepend_args = { "-i", "2" },
-      },
-      ruff_fix = {
-        prepend_args = { "check" },
-      }
-    },
-  },
-  init = function()
-    -- If you want the formatexpr, here is the place to set it
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	-- Everything in opts will be passed to setup()
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = {
+				-- To fix lint errors.
+				"ruff_fix",
+				-- To run the Ruff formatter.
+				"ruff_format",
+			},
+			sql = { "sql_formatter" },
+		},
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500, lsp_fallback = true },
+		-- Customize formatters
+		formatters = {
+			shfmt = {
+				prepend_args = { "-i", "2" },
+			},
+			ruff_fix = {
+				prepend_args = { "check" },
+			},
+			sql_formatter = {
+				prepend_args = {
+					"-c",
+					'{"keywordCase": "upper", "dataTypeCase": "upper", "linesBetweenQueries": 1}',
+				},
+			},
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 }
