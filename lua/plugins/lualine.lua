7ca842ae83ddcfac37bfa3ca81e7902d6e86896e
Jad Haddad <jad.haddad92@gmail.com>
2024-12-02 17:51:50 +0100

    Add noice plugin to replace status bar with UI

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -36,7 +36,15 @@ return {
 				},
 			},
 
-			lualine_x = { "encoding", "filetype" },
+			lualine_x = {
+				{
+					require("noice").api.statusline.mode.get,
+					cond = require("noice").api.statusline.mode.has,
+					color = { fg = "#ff9e64" },
+				},
+				"encoding",
+				"filetype",
+			},
 		},
 	},
 }
