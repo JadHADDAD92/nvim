c49216a9fa08658ca8a9350ed3fe47b574f52bb7
Jad Haddad <jad.haddad92@gmail.com>
2024-12-19 17:56:07 +0100

    remove noice

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -37,11 +37,6 @@ return {
 			},
 
 			lualine_x = {
-				{
-					require("noice").api.statusline.mode.get,
-					cond = require("noice").api.statusline.mode.has,
-					color = { fg = "#ff9e64" },
-				},
 				"encoding",
 				"filetype",
 			},
