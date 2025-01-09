52fa7c01fffb965462fdfcf54eb3516f9a37de5f
Jad Haddad <jad.haddad92@gmail.com>
2025-01-09 15:44:15 +0100

    Revert "Replace nvim-cmp with blink.cmp"
    This reverts commit add578a41df3030e4f7b74575e51ad63b5b37b4c.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,26 +1,26 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      filetypes = {
-        ["*"] = true,
-      },
-      panel = { enabled = true },
-      suggestion = {
-        auto_trigger = true,
-        keymap = {
-          next = "<C-n>",
-          prev = "<C-p>",
-          accept = "<C-y>",
-          accept_word = "<M-u>",
-          auto_complete = "<C-space>",
-        },
-      },
-      inline_suggestion = { enabled = true },
-      jump_to_mark = { enabled = true },
-      ghost_text = { enabled = true },
-    })
-  end,
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			filetypes = {
+				["*"] = true,
+			},
+			panel = { enabled = true },
+			suggestion = {
+				auto_trigger = true,
+				keymap = {
+					next = "<C-n>",
+					prev = "<C-p>",
+					accept = "<C-y>",
+					accept_word = "<M-u>",
+					auto_complete = "<C-space>",
+				},
+			},
+			inline_suggestion = { enabled = true },
+			jump_to_mark = { enabled = true },
+			ghost_text = { enabled = true },
+		})
+	end,
 }
